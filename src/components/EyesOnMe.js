b73315d192ab00b049c712cc0362b64a1f300108
Yahya-Mohamed-1 <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-09-06 16:30:04 +0300

    Lab submission

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+function  EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={()=>console.log('Good!')} onBlur={()=>console.log('Hey! Eyes on me!')}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
